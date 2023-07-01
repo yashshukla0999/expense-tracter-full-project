6c1e40cb4687395f5f6a73dc3294efc433a179bb
yashshukla099 <yashshukla0999@gmail.com>
2023-07-01 15:43:45 +0530

    hide the password in table

--- controller/controls.js
+++ controller/controls.js
@@ -1,46 +1,52 @@
 const path = require('path');
 const Expense = require('../models/data');
+const bcrypt = require('bcrypt');
 
 
-exports.showForm=(req,resp)=>{
-    
-    resp.sendFile(path.join(__dirname,'../views/index.html'));
+exports.showForm = (req, resp) => {
+
+  resp.sendFile(path.join(__dirname, '../views/index.html'));
+}
+
+
+exports.postUser = (req, resp) => {
+  const userName = req.body.name;
+  const userEmail = req.body.email;
+  const userPassword = req.body.password;
+  try {
+    if (userName == undefined || userName.length === 0 || userEmail.length === 0 ||
+      userEmail == undefined
+      || userPassword.length === 0 || userPassword == undefined) {
+      return resp.status(400).json({ err: "bad parameters" })
     }
+    bcrypt.hash(userPassword, 10, async (err, hash) => {
+      console.log(err)
+      await Expense.create({
+        name: userName,
+        email: userEmail,
+        password: hash
+      })
+      resp.status(201).json({ massage: 'user created successfully' });
+
+    })
+  }
+
+  catch (err) {
+    console.log('Error:', err);
+    resp.status(500).json({ err: 'email already register' });
+  };
 
 
-exports.postUser=(req,resp)=>{
-    const userName = req.body.name;
-    const userEmail = req.body.email;
-    const userPassword=req.body.password;
-    if(userName == undefined || userName.length ===0 || userEmail.length ===0|| 
-      userEmail == undefined  
-      ||userPassword.length ===0||userPassword == undefined){
-        return resp.status(400).json({err:"bad parameters"})
-      }
-        
-        Expense.create({
-          name: userName,
-          email: userEmail,
-          password: userPassword
-        })
 
-          .then((result) => {
-            console.log(result);
-            resp.status(201).json({massage:'user created successfully'});
-          })
-          .catch((err) => {
-            console.log('Error:', err);
-            resp.status(500).json({err:'email already register'});
-          });
-      }
+}
 
 
 
 
 
-      exports.postUserLogin=(async (req,resp)=>{
-       const userEmail = req.body.LoginEmail;
-      const userPassword = req.body.LoginPassword;
+exports.postUserLogin = async (req, resp) => {
+  const userEmail = req.body.LoginEmail;
+  const userPassword = req.body.LoginPassword;
 
   try {
     const user = await Expense.findOne({
@@ -53,20 +59,19 @@ exports.postUser=(req,resp)=>{
       return resp.status(404).json({ error: "User not found" });
     }
 
-    if (user.email != userEmail) {
-      return resp.status(401).json({ error: "Incorrect email" });
-    }
-  
-    if (user.password != userPassword) {
-      return resp.status(401).json({ error: "Incorrect password" });
-    }
-  
-    resp.status(200).json({ message: "User logged in successfully" });
+    bcrypt.compare(userPassword, user.password, (error, result) => {
+      if (error) {
+        return resp.status(500).json({ error: "Internal server error" });
+      }
 
-  }
-   catch (err) {
+      if (!result) {
+        return resp.status(401).json({ error: "Incorrect password" });
+      }
+
+      resp.status(200).json({ message: "User logged in successfully" });
+    });
+  } catch (err) {
     console.log("Error:", err);
     resp.status(500).json({ error: "Internal server error" });
   }
-
-    })
+};
